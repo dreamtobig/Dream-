e64b67ca8d1a510d921bbecec14afe17ba652467
dreamtobig <mydream2big@gmail.com>
2024-12-25 00:48:09 +0100

    Add files via upload

---
+++ README.md
@@ -0,0 +1,4 @@
+# BIG DREAM 
+
+A Pen created on CodePen.io. Original URL: [https://codepen.io/Biggest-Dream-/pen/zxOzjEX](https://codepen.io/Biggest-Dream-/pen/zxOzjEX).
+
---
+++ script.js
@@ -0,0 +1,4 @@
+// Add interactivity here, e.g., a responsive navbar toggle
+document.addEventListener("DOMContentLoaded", () => {
+  console.log("BIG DREAM Website Ready");
+});
